c9429e56a0aece8a6e9ac9777a316349fb2b88cb
Miskat Mahmud <115903852+codedsami@users.noreply.github.com>
2025-03-25 04:21:21 -0400

    changing everything back to original because I forgot to take screenshots

--- src/test/java/soen/tutorial/ArithmeticOperationTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationTest.java
@@ -3,7 +3,7 @@ package soen.tutorial;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+//import static org.junit.Assert.fail;
 
 public class ArithmeticOperationTest {
 
@@ -15,19 +15,19 @@ public class ArithmeticOperationTest {
 		ArithmeticOperation operations = new ArithmeticOperation();
 		Integer actual = operations.addOrSub(2, 6);
 		Integer expected = 8;
-		//assertEquals(expected, actual);
-		fail("must fail");
+		assertEquals(expected, actual);
+		//fail("must fail");
 
 	}
-    // @Test
-    // public void testSub()
-    // {
+    @Test
+    public void testSub()
+    {
 
-    //     ArithmeticOperation operations = new ArithmeticOperation();
-    //     Integer actual = operations.addOrSub(6, 2);
-    //     Integer expected = 4;
-    //     assertEquals(expected, actual);
+        ArithmeticOperation operations = new ArithmeticOperation();
+        Integer actual = operations.addOrSub(6, 2);
+        Integer expected = 4;
+        assertEquals(expected, actual);
 
-    // }
+    }
 
 }
